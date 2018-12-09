ab7e278686ba99c808497049b4eb9d7b5ebedbd3
Nikolai <dancingdar@mail.ru>
2018-12-09 14:03:50 +0300

    Delete comments

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <thread>
 #include "Single.h"
 #include "Multi.h"
-#include "pcg_random.hpp"
 
 int main() {
     pcg64 rng(1);
@@ -13,46 +12,9 @@ int main() {
      * Генерация
      */
     clock_t start;
-//    for (int i = 0; i < K; ++i) {
-//        t1.emplace_back(std::thread(generate, std::ref(m_res), i));
-//    }
-//
-//    for (auto &t : t1) {
-//        t.join();
-//    }
-//
-//    std::cout << "Generation of sample of " << N << " random numbers in " << K << " threads took: "
-//              << float(clock() - start) << std::endl;
 
 
     std::vector<double> s_res(N);
-//    start = clock();
-//    single_generate(s_res, rng);
-//
-//    std::cout << "Generation of sample of " << N << " random numbers in single thread took: "
-//              << float(clock() - start) << std::endl << std::endl;
-//
-//    /**
-//     * Вычисление экспоненты
-//     */
-
-//    start = clock();
-//    for (int i = 0; i < K; ++i) {
-//        t2.emplace_back(std::thread(calculate, std::ref(m_res), i));
-//    }
-//
-//    for (auto &t : t2) {
-//        t.join();
-//    }
-//
-//    std::cout << "Calculation exp of sample of " << N << " random numbers in " << K << " threads took: "
-//              << float(clock() - start) << std::endl;
-
-//    start = clock();
-//    single_calculate(s_res);
-//    std::cout << "Calculation exp of sample of " << N << " random numbers in single thread took: "
-//              << float(clock() - start) << std::endl << std::endl;
-//
 
     /**
      * Подсчет интеграла
@@ -83,6 +45,5 @@ int main() {
               << float(clock() - start) << std::endl << std::endl;
     std::cout << "Sample mean multi = " << m_mean << std::endl;
     std::cout << "Sample mean single = " << s_mean << std::endl;
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
